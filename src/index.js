bd146eb46ba494776b5cec5906d6ccbd954d8e9e
Bunny-buzz <Bunny-buzz@users.noreply.github.com>
2020-07-16 20:06:41 -0600

    Costructed JS and added a broken image to HTML

---
+++ src/index.js
@@ -0,0 +1,23 @@
+let now = new Date();
+let timepiece = document.querySelector("#timepiece");
+let date = now.getDate();
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+timepiece.innerHTML = `${day} ${date}, ${hours}: ${minutes}`;
