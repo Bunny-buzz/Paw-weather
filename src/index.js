5610a6ee1dd396439ada01a36223c24599a6ad4c
Bunny-buzz <Bunny-buzz@users.noreply.github.com>
2020-07-20 17:27:58 -0600

    Changed showWeather to displayTemperature

--- src/index.js
+++ src/index.js
@@ -106,9 +106,9 @@ function retrievePosition(position) {
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
   let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
-  axios.get(url).then(showWeather);
+  axios.get(url).then(displayTemperature);
 }
 
 navigator.geolocation.getCurrentPosition(retrievePosition);
 
-search("Calgary");
+search("Fort Mcmurray");
